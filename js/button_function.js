89d8ad30290048fb476efd886250587f26dfadda
动空空空空 <82658163+SkyDynamic@users.noreply.github.com>
2023-05-28 12:36:25 +0800

    Add files via upload

---
+++ js/button_function.js
@@ -0,0 +1,17 @@
+const img_path = "static/img/{0}.webp"
+
+if (!String.prototype.format) {
+    String.prototype.format = function() {
+      var args = arguments;
+      return this.replace(/{(\d+)}/g, function(match, number) { 
+        return typeof args[number] != 'undefined'
+          ? args[number]
+          : match
+        ;
+      });
+    };
+}
+
+function switch_img(vaule){
+  $('#pay_img').attr('src', img_path.format(vaule))
+}
